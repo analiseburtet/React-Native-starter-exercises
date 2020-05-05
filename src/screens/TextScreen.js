27b7df57f24c5ee0a753541e5c9473aed40d32f9
analiseburtet <analiseburtet@gmail.com>
2020-05-05 12:25:55 -0300

    [S06C62] - Two important props

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,24 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
 
 const TextScreen = () => {
     return(
         <View>
-            <Text>text screen</Text>
+            <TextInput
+                autoCorrect={false}
+                style={styles.input}
+                autoCapitalize="none"
+            />        
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    input: {
+        margin: 15,
+        borderColor: 'black',
+        borderWidth: 1
+    }
+})
 
 export default TextScreen

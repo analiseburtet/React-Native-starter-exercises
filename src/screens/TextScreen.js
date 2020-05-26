379823b5551bceea230bd9bd3e5922519cf160a1
analiseburtet <analiseburtet@gmail.com>
2020-05-26 11:43:54 -0300

    [S06C65] - Exercise Outline]

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -2,18 +2,25 @@ import React, {  useState } from 'react'
 import { View, Text, StyleSheet, TextInput } from 'react-native'
 
 const TextScreen = () => {
-    const [  name, setName ] = useState('')
+    const [  password, setPassword ] = useState('')
+    const [ validation, setValidation ] = useState(true)
 
     return(
         <View>
-            <Text>Name: {name}</Text>
+            <Text>Enter password: </Text>
             <TextInput
+                type="password"
                 autoCorrect={false}
                 style={styles.input}
                 autoCapitalize="none"
-                value={name}
-                onChangeText={(newValue) => setName(newValue)}
-            />        
+                value={password}
+                onChangeText={(newValue) => {
+                        newValue.length <= 5 ? setValidation(true) : setValidation(false)
+                        setPassword(newValue)
+                    }
+                }
+            />     
+            { validation ? <Text>Password must be longer than 5 characteres </Text> : null}   
         </View>
     )
 }
